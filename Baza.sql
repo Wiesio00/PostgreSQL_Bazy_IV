092067a693b9e025237c66570cd4a3e09940578b
Wiesio00 <wiesio00@gmail.com>
2018-05-28 22:46:26 +0200

    Baza.sql

---
+++ Baza.sql
@@ -0,0 +1,10 @@
+--PROJEKT na Bazy Danych IV.
+/*Sklad osobowy
+Paweł Wiejak 5762
+Paweł Mańskowski 5123
+Bartek Górski 5070
+
+Oficjalnie do naszej grupy dopisany był Artur Kwiatkowski
+ale nie udało nam się z nim skontaktować.
+*/
+
